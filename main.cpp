62ab757a8a98c1fd02e9eabc34357d6bc96de36c
Pasindu115530 <pasinduudana12m2@gmail.com>
2024-07-08 02:26:34 +0530

    First version of snake_game

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x,y,fruitX,fruitY,score;
+enum eDirection { STOP = 0 , LEFT , UP , DOWN };
+eDirection  dir;
+void setup(){
+    gameOver = false;
+    dir = STOP;
+    x = width/2;
+    y = height/2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+
+}
+void Draw()
+{
+    system("cls");
+    for (int i = 0 ; i < width ; i++)
+        cout << "#";
+
+}
+void Input(){
+
+}
+void Logic(){
+
+}
+int main(){
+
+    setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+    }
+    
+
+}
