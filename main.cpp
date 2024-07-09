da69277da4463b00a0e121b283fd7fafe595a7f0
Pasindu115530 <pasinduudana12m2@gmail.com>
2024-07-09 23:56:35 +0530

    Final cpp file and txt file

--- main.cpp
+++ main.cpp
@@ -1,41 +1,82 @@
-#include<iostream>
-#include<conio.h>
-#include<windows.h>
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <fstream>
 
 using namespace std;
+
 bool gameOver;
 const int width = 20;
 const int height = 20;
-int x,y,fruitX,fruitY,score;
-int tailX[100] , tailY[100];
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
 int nTail;
-enum eDirection { STOP = 0 , LEFT ,RIGHT, UP , DOWN };
-eDirection  dir;
-void setup(){
+int highscore;
+string player;
+string playerName;
+
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+
+void setup() {
     gameOver = false;
     dir = STOP;
-    x = width/2;
-    y = height/2;
+    x = width / 2;
+    y = height / 2;
     fruitX = rand() % width;
     fruitY = rand() % height;
     score = 0;
-
+    nTail = 0;
 }
-void Draw()
-{
-    system("cls");
-    for (int i = 0 ; i < width+2 ; i++)
-        cout << "#";
-        cout << endl;
+void loadPlayer(){
+        ifstream file("player.txt");
+    if (file.is_open()) {
+        file >> player;
+        file.close();
+    } else {
+        player = " "; // If no high score file exists, start with 0
+    }
+}
+void savePlayer() {
+    ofstream file("player.txt");
+    if (file.is_open()) {
+        file << player;
+        file.close();
+    }
+}
 
-    for (int i = 0 ; i < height ; i++){
-        for (int j = 0 ; j < width ; j++) {
-            if (j == 0) 
-            cout <<"#";
+void loadHighScore() {
+    ifstream file("highscore.txt");
+    if (file.is_open()) {
+        file >> highscore;
+        file.close();
+    } else {
+        highscore = 0; // If no high score file exists, start with 0
+    }
+}
+
+void saveHighScore() {
+    ofstream file("highscore.txt");
+    if (file.is_open()) {
+        file << highscore;
+        file.close();
+    }
+}
+
+void Draw() {
+    system("cls");
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                cout << "#";
             if (i == y && j == x)
-            cout << "O";
-            else if ( i == fruitY && j == fruitX)
-            cout << "F";
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
             else {
                 bool print = false;
                 for (int k = 0; k < nTail; k++) {
@@ -46,50 +87,44 @@ void Draw()
                 }
                 if (!print) cout << " ";
             }
-                
-            if (j == width-1)
-            {
-            cout <<"#";
+
+            if (j == width - 1) {
+                cout << "#";
             }
         }
         cout << endl;
-    }   
-        for (int i = 0 ; i < width+2 ; i++)
-        cout << "#" ;
-        cout << endl;
-        cout << "score  =  "  << score;        
-
-    
-        
-
+    }
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    cout << "Score = " << score << endl;
+    cout << "High Score = " << highscore << endl;
+    cout << "Best Player = " << player << endl;
 }
-void Input(){
-    if (_kbhit())
-    {
-       switch(_getch())
-        {
-            case 'a' :
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+            case 'a':
                 dir = LEFT;
                 break;
-            case 'd' :
+            case 'd':
                 dir = RIGHT;
-                break;   
-            case 'w' :
+                break;
+            case 'w':
                 dir = UP;
-                break;                   
-            case 's' :
+                break;
+            case 's':
                 dir = DOWN;
-                break; 
-            case 'x' :
+                break;
+            case 'x':
                 gameOver = true;
-                break;     
+                break;
         }
     }
-    
-
 }
-void Logic()
-{    
+
+void Logic() {
     int prevX = tailX[0];
     int prevY = tailY[0];
     int prev2X, prev2Y;
@@ -101,7 +136,8 @@ void Logic()
         tailX[i] = prevX;
         tailY[i] = prevY;
         prevX = prev2X;
-        prevY = prev2Y;}
+        prevY = prev2Y;
+    }
     switch (dir) {
         case LEFT:
             x--;
@@ -120,27 +156,32 @@ void Logic()
     }
     if(x > height || x < -1 || y > width || y < -1 )
         gameOver = true;
-    if(x==fruitX && y==fruitY) {
+
+    if (x == fruitX && y == fruitY) {
         score += 10;
         fruitX = rand() % width;
         fruitY = rand() % height;
         nTail++;
-
-    }   
-
-
+        if (score > highscore) {
+            highscore = score;
+            player = playerName;
+        }
+    }
 }
-int main(){
 
+int main() {
+    cout << "Enter your name: ";
+    cin >> playerName;
     setup();
-    while (!gameOver)
-    {
+    loadHighScore();
+    loadPlayer();
+    while (!gameOver) {
         Draw();
         Input();
         Logic();
-        Sleep(50);
-        
+        Sleep(100);
     }
-    
-
+    saveHighScore();
+    savePlayer();
+    return 0;
 }
